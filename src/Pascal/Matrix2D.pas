14db44356d39bed8d54b7ac92e2533195eb44115
Dmitry Zotikov <xio@ungrund.org>
2013-11-28 23:57:04 +0400

    Matrix2D: +LUDecomposition, +Determinant, +ForwardBackwardSubst, +Eye

--- src/Pascal/Matrix2D.pas
+++ src/Pascal/Matrix2D.pas
@@ -9,19 +9,30 @@ uses
 
 type 
 	Matrix = array of array of integer;
+	RealMatrix = array of array of real;
 	TIntDynArray = array of integer;
+	TRealDynArray = array of real;
+	MatrixTuple = array [0..1] of RealMatrix;
 
 
 procedure PrintArray(A: array of LongInt; sep: string = #10);
 procedure PrintArray(A: TStringDynArray; sep: string = #10);
+procedure PrintArray(A: array of real; sep: string = #10);
 procedure Print2DMatrix(A: Matrix; sep: string = ' ');
+procedure Print2DMatrix(A: RealMatrix; sep: string = ' ');
 function Gen2DMatrix(m: integer = 3; n: integer = 0; RMax: integer = 10): Matrix;
 function SumArray(A: array of LongInt): integer;
 function StringToIntArray(S: string; sep: char = ' '): TIntDynArray;
 function Transpose(A: Matrix): Matrix;
 function Multiply(A: Matrix; B: Matrix): Matrix;
+function Multiply(A: Matrix; B: RealMatrix): RealMatrix;
 function Multiply(A: Matrix; c: integer): Matrix;
 procedure InitMatrix(var A: Matrix; m: integer; n: integer = 0);
+procedure InitMatrix(var A: RealMatrix; m: integer; n: integer = 0);
+function LUDecomposition(A: Matrix): MatrixTuple;
+function Determinant(A: Matrix): real;
+function ForwardBackwardSubst(L: RealMatrix; U: RealMatrix; B: TRealDynArray): TRealDynArray;
+function Eye(n: integer): Matrix;
 
 
 
@@ -51,7 +62,26 @@ begin
 	writeln('');
 end;
 
+procedure PrintArray(A: array of real; sep: string = #10); overload;
+var
+	i: integer;
+begin
+	for i:=Low(A) to High(A) do
+		write(A[i], sep);
+	writeln('');
+end;
+
 procedure Print2DMatrix(A: Matrix; sep: string = ' ');
+var
+	i: integer;
+begin
+	for i:=Low(A) to High(A) do
+	begin
+		PrintArray(A[i], sep);
+	end;
+end;
+
+procedure Print2DMatrix(A: RealMatrix; sep: string = ' '); overload;
 var
 	i: integer;
 begin
@@ -102,7 +132,7 @@ begin
 	StringToIntArray := Res;
 end;
 
-procedure InitMatrix(var A: Matrix; m: integer; n: integer = 0);
+procedure InitMatrix(var A: Matrix; m: integer; n: integer = 0); overload;
 var
 	i: integer;
 begin
@@ -113,6 +143,19 @@ begin
 		else
 			SetLength(A[i], n);
 end;
+
+procedure InitMatrix(var A: RealMatrix; m: integer; n: integer = 0); overload;
+var
+	i: integer;
+begin
+	SetLength(A, m);
+	for i:=0 to (m-1) do
+		if n = 0 then
+			SetLength(A[i], m)
+		else
+			SetLength(A[i], n);
+end;
+
 
 function Transpose(A: Matrix): Matrix;
 var
@@ -158,6 +201,32 @@ begin
 	Multiply := C;
 end;
 
+function Multiply(A: Matrix; B: RealMatrix): RealMatrix;	overload;
+var
+	C: RealMatrix;
+	m_A, m_B, n_A, n_B, i, j, k: integer;
+begin
+	m_A := Length(A); 
+	m_B := Length(B); 
+	n_A := Length(A[0]); 
+	n_B := Length(B[0]); 
+
+	if (m_A <> n_B) or (n_A <> m_B) then
+	begin
+		writeln('Can''t multiply matrices [', m_A, ' x ', n_A, '] and [', m_B, ' x ', n_B, ']');
+		halt();
+	end;
+
+	InitMatrix(C, m_A);
+
+	for i := 0 to (m_A - 1) do
+		for j := 0 to (n_B - 1) do
+			for k := 0 to (n_A - 1) do
+				C[i][j] := C[i][j] + A[i][k] * B[k][j];
+
+	Multiply := C;
+end;
+
 function Multiply(A: Matrix; c: integer): Matrix;	overload;
 var
 	i, j: integer;
@@ -169,6 +238,155 @@ begin
 	Multiply := A;
 end;
 
+function LUDecomposition(A: Matrix): MatrixTuple;
+{ Make sure the matrix is square and non-singular }
+{ Basically, following the algorithm from http://ru.math.wikia.com/wiki/LU-разложение }
+var
+	L, U: RealMatrix;
+	LU: MatrixTuple;
+	i, j, k, n: integer;
+	acc: real;
+begin
+	n := Length(A);
+	InitMatrix(L, n);
+	InitMatrix(U, n);
+
+	if A[0, 0] = 0 then
+	begin
+		writeln('a_11 is zero, cannot decompose');
+		exit;
+	end;
+
+	for i := 0 to n-1 do
+		for j := 0 to n-1 do
+		begin
+			U[0, i] := A[0, i];
+			L[i, 0] := A[i, 0] / U[0, 0];
+			acc := 0;
+			for k := 0 to i-1 do
+				acc := acc + L[i, k] * U[k, j];
+			U[i, j] := A[i, j] - acc;
+			if i > j then
+				L[j, i] := 0
+			else
+			begin
+				acc := 0;
+				for k := 0 to i-1 do
+					acc += L[j, k] * U[k, i];
+				L[j, i] := (A[j, i] - acc) / U[i, i];
+			end;
+		end;
+
+	LU[0] := L;
+	LU[1] := U;
+
+	LUDecomposition := LU;
+end;
+
+{ function LUDecomposition(A: Matrix): RealMatrix; }
+{ [> Make sure the matrix is square and non-singular <] }
+{ [> Following http://ru.wikipedia.org/wiki/LU-разложение, but this algorithm doesn't seem to work <] }
+{ var }
+	{ LU: RealMatrix; }
+	{ i, j, k, n: integer; }
+	{ acc: real; }
+{ begin }
+	{ n := Length(A); }
+	{ InitMatrix(LU, n); }
+
+	{ for j := 0 to n-1 do }
+	{ begin }
+		{ LU[0, j] := A[0, j]; }
+		{ LU[j, 0] := A[j, 0] / LU[0, 0]; }
+	{ end; }
+
+	{ for i := 1 to n-1 do }
+	{ begin }
+		{ for j := i to n-1 do }
+		{ begin }
+			{ acc := 0; }
+			{ for k := 0 to (i-2) do }
+				{ acc := acc + LU[i, k] * LU[k, j]; }
+			{ LU[i, j] := A[i, j] - acc; }
+		{ end; }
+
+		{ for j := i to n-1 do }
+		{ begin }
+			{ acc := 0; }
+			{ for k := 0 to (i-2) do }
+				{ acc := acc + LU[j, k] * LU[k, i]; }
+			{ LU[j, i] := 1/LU[i, i] * (LU[j, i] - acc); }
+		{ end; }
+	{ end; }
+
+	{ LUDecomposition := LU; }
+{ end; }
+
+function Determinant(A: Matrix): real;
+var
+	LU: MatrixTuple;
+	det: real;
+	i: integer;
+begin
+	LU := LUDecomposition(A);
+	det := 1;
+	for i := 0 to Length(LU[1])-1 do
+	begin
+		det := det * LU[1][i, i];
+	end;
+
+	Determinant := det;
+end;
+
+function ForwardBackwardSubst(L: RealMatrix; U: RealMatrix; B: TRealDynArray): TRealDynArray;
+{ Following http://en.wikipedia.org/wiki/Triangular_matrix#Forward_and_back_substitution }
+var
+	i, j, n: integer;
+	X, Y: TRealDynArray;
+begin
+	{ Ax = b -> LUx = b. Then y is defined to be Ux }
+	i := 0;
+	j := 0;
+	n := Length(b);
+	SetLength(X, n);
+	SetLength(Y, n);
+	{ Forward solve Ly = b }
+	for i := 0 to n-1 do
+	begin
+		Y[i] := B[i];
+		for  j := 0 to i-1 do
+			Y[i] := Y[i] - L[i, j] * Y[j];
+		Y[i] := Y[i] / L[i, i];
+	end;
+	{ Backward solve Ux = y }
+	for i := n-1 downto 0 do
+	begin
+		X[i] := Y[i];
+		for  j := i + 1 to n-1 do
+			X[i] := X[i] - U[i, j] * X[j];
+		X[i] := X[i] / U[i, i];
+	end;
+
+	ForwardBackwardSubst := X;
+end;
+
+function Eye(n: integer): Matrix;
+var
+	j, k: integer;
+	I: Matrix;
+begin
+	SetLength(I, n);
+	for j := 0 to n-1 do
+	begin
+		SetLength(I[j], n);
+		for k := 0 to n-1 do
+			if j = k then
+				I[j, k] := 1
+			else
+				I[j, k] := 0;
+	end;
+	Eye := I;
+end;
 
 { initialization }
 
--- src/Pascal/MatrixTest.pas
+++ src/Pascal/MatrixTest.pas
@@ -5,34 +5,104 @@ uses Matrix2D;
 var
 	A: Matrix;
 	B: Matrix;
+	A_inv: RealMatrix;
+	LU: array [0..1] of RealMatrix;
 	m, n, c: integer;
+	F, X: TRealDynArray;
+	i, j: integer;
 
 begin
-	write('m = ');
-	read(m);
-	write('n = ');
-	read(n);
-	A := Gen2DMatrix(m, n);
-	Print2DMatrix(A);
-	writeln();
+	{ write('m = '); }
+	{ read(m); }
+	{ write('n = '); }
+	{ read(n); }
+	{ A := Gen2DMatrix(m, n); }
+	{ Print2DMatrix(A); }
+	{ writeln(); }
 
-	writeln('A^T:');
-	Print2DMatrix(Transpose(A));
-	writeln();
+	{ writeln('A^T:'); }
+	{ Print2DMatrix(Transpose(A)); }
+	{ writeln(); }
 
-	B := Gen2DMatrix(n, m);
-	Print2DMatrix(A);
-	writeln('x');
-	Print2DMatrix(B);
-	writeln('---');
-	Print2DMatrix(Multiply(A, B));
-	writeln();
+	{ B := Gen2DMatrix(n, m); }
+	{ Print2DMatrix(A); }
+	{ writeln('x'); }
+	{ Print2DMatrix(B); }
+	{ writeln('---'); }
+	{ Print2DMatrix(Multiply(A, B)); }
+	{ writeln(); }
 
-	write('c (in ZZ) = ');
-	read(c);
-	writeln(c);
-	writeln('*');
+	{ write('c (in ZZ) = '); }
+	{ read(c); }
+	{ writeln(c); }
+	{ writeln('*'); }
+	{ Print2DMatrix(A); }
+	{ writeln('---'); }
+	{ Print2DMatrix(Multiply(A, c)); }
+
+	{ writeln('LU-decomposition'); }
+	{ write('n = '); }
+	{ read(n); }
+	{ A := Gen2DMatrix(n, n); }
+	{ Print2DMatrix(A); }
+	{ LU := LUDecomposition(A); }
+	{ writeln('L = '); }
+	{ Print2DMatrix(LU[0]); }
+	{ writeln('U = '); }
+	{ Print2DMatrix(LU[1]); }
+	{ writeln(); }
+	{ writeln('det: ', Determinant(A)); }
+	{ writeln(); }
+
+	{ writeln('Forward-backward substitution'); }
+	{ InitMatrix(A, 3); }
+	{ A[0, 0] := 1; }
+	{ A[0, 1] := 0; }
+	{ A[0, 2] := 2; }
+	{ A[1, 0] := 2; }
+	{ A[1, 1] := -1; }
+	{ A[1, 2] := 3; }
+	{ A[2, 0] := 4; }
+	{ A[2, 1] := 1; }
+	{ A[2, 2] := 8; }
+	{ Print2DMatrix(A); }
+	{ LU := LUDecomposition(A); }
+	{ SetLength(F, 3); }
+	{ F[0] := -4; }
+	{ F[1] := -6; }
+	{ F[2] := -15; }
+	{ writeln('F:'); }
+	{ PrintArray(F); }
+	{ X := ForwardBackwardSubst(LU[0], LU[1], F); }
+	{ PrintArray(X); }
+
+	writeln('Inverse');
+	InitMatrix(A, 3);
+	InitMatrix(A_inv, 3);
+	A[0, 0] := 1;
+	A[0, 1] := 0;
+	A[0, 2] := 2;
+	A[1, 0] := 2;
+	A[1, 1] := -1;
+	A[1, 2] := 3;
+	A[2, 0] := 4;
+	A[2, 1] := 1;
+	A[2, 2] := 8;
 	Print2DMatrix(A);
-	writeln('---');
-	Print2DMatrix(Multiply(A, c));
+	LU := LUDecomposition(A);
+	SetLength(F, Length(A));
+	for i := 0 to Length(A)-1 do
+	begin
+		for j := 0 to Length(A)-1 do
+			F[j] := Eye(Length(A))[j][i];
+		X := ForwardBackwardSubst(LU[0], LU[1], F);
+		for j := 0 to Length(A)-1 do
+			A_inv[j][i] := X[j];
+	end;
+	Print2DMatrix(A_inv);
+	writeln('Multiplying A to A_inv...');
+	Print2DMatrix(Multiply(A, A_inv));
+
+	{ writeln('Eye (5): '); }
+	{ Print2DMatrix(Eye(5)); }
 end.
